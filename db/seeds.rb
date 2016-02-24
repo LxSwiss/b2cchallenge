35d0bccc38d1599d267a88efa7bfc152a1b752d4
Alexander Schmidt <a.schmidt@apoveda.ch>
2016-02-24 10:13:28 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Challenge.delete_all
 
-Challenge.create!(title: 'Do a 360 with a snowboard', description: %{
-Get 30% off if you do a 360 with a snowboard
+Challenge.create!(title: 'Mache einen 180 auf der Piste', description: %{
+Filme dich wie du einen 180 mit deinem Snowboard oder Skis machst und erhalte 30% Rabatt auf deinen nächsten Einkauf bei Stökli
+}, image_url: 'Stoeckli.jpeg')
+
+Challenge.create!(title: 'Freibier an Patricks Day', description: %{
+Trage grüne Kleider an Patricks Day und erhalte ein Freibier bei Nelson}, image_url: 'nelson.jpg')
+
+Challenge.create!(title: 'Mache einen Schneeengel', description: %{
+Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
+}, image_url: 'Mammut.jpg')
+
+Challenge.create!(title: 'Erhalte 0.5% Rabatt auf jeden Kilometer in deinem Lauf', description: %{
+Mache einen Lauf und erhalte 0.5% Rabatt auf jeden Kilometer für das ganze Nike Sortiment
 }, image_url: 'Old_Nike_logo.jpg')
 
-Challenge.create!(title: 'Free beer on Patricks day', description: %{
-If you wear green clothes you get a free beer on patricks day
-}, image_url: 'Old_Nike_logo.jpg')
+Challenge.create!(title: 'Mache ein Selfie auf einem Hügel', description: %{
+Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
+}, image_url: 'SBB.png')
 
-Challenge.create!(title: 'Free beer on Patricks day', description: %{
-If you wear green clothes you get a free beer on patricks day
-}, image_url: 'Old_Nike_logo.jpg')
+Challenge.create!(title: 'Mache ein Selfie auf einem Eisfeld', description: %{
+Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
+}, image_url: 'ochsner.png')
+
+Challenge.create!(title: 'Mache ein Selfie am Strand', description: %{
+Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
+}, image_url: 'ochsner.png')
