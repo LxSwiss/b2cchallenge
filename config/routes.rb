6837b72a6c5b264e4f7fc6b120529d039a84ba95
Alexander Schmidt <a.schmidt@apoveda.ch>
2016-02-26 15:40:26 +0100

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'challengestore#index', as: 'challangestore'
+  root 'challengestore#index', as: 'challengestore'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
