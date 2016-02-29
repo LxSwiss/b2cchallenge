eebf21711b1e9e89e4a1145dd46ef10e2efb1d23
Alexander Schmidt <lxswiss@gmail.com>
2016-02-29 23:55:17 +0100

    changes on layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,27 +17,31 @@ Challenge.delete_all
 
 Challenge.create!(title: 'Mache einen 180 auf der Piste', description: %{
 Filme dich wie du einen 180 mit deinem Snowboard oder Skis machst und erhalte 30% Rabatt auf deinen nächsten Einkauf bei Stökli
-}, image_url: 'Stoeckli.jpeg')
+}, image_url: 'lighthouse.jpg')
 
 Challenge.create!(title: 'Freibier an Patricks Day', description: %{
-Trage grüne Kleider an Patricks Day und erhalte ein Freibier bei Nelson}, image_url: 'nelson.jpg')
+Trage grüne Kleider an Patricks Day und erhalte ein Freibier bei Nelson}, image_url: 'run.jpg')
 
 Challenge.create!(title: 'Mache einen Schneeengel', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'Mammut.jpg')
+}, image_url: 'lighthouse.jpg')
 
 Challenge.create!(title: 'Erhalte 0.5% Rabatt auf jeden Kilometer in deinem Lauf', description: %{
 Mache einen Lauf und erhalte 0.5% Rabatt auf jeden Kilometer für das ganze Nike Sortiment
-}, image_url: 'Old_Nike_logo.jpg')
+}, image_url: 'run.jpg')
 
 Challenge.create!(title: 'Mache ein Selfie auf einem Hügel', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'SBB.png')
+}, image_url: 'lighthouse.jpg')
 
 Challenge.create!(title: 'Mache ein Selfie auf einem Eisfeld', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'ochsner.png')
+}, image_url: 'run.jpg')
 
 Challenge.create!(title: 'Mache ein Selfie am Strand', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'ochsner.png')
+}, image_url: 'run.jpg')
+
+Challenge.create!(title: 'Lorem Ipsum', description: %{
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+}, image_url: 'lighthouse.jpg')
