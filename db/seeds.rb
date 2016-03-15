4f7944ee33069819cee862ac26173242b3561729
Alexander Schmidt <lxswiss@gmail.com>
2016-03-15 11:49:57 +0100

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ Challenge.delete_all
 
 Challenge.create!(title: 'Mache einen 180 auf der Piste', description: %{
 Filme dich wie du einen 180 mit deinem Snowboard oder Skis machst und erhalte 30% Rabatt auf deinen nächsten Einkauf bei Stökli
-}, image_url: 'lighthouse.jpg')
+}, image_url: 'snowboard.jpg')
 
 Challenge.create!(title: 'Freibier an Patricks Day', description: %{
-Trage grüne Kleider an Patricks Day und erhalte ein Freibier bei Nelson}, image_url: 'run.jpg')
+Trage grüne Kleider an Patricks Day und erhalte ein Freibier bei Nelson}, image_url: 'feier.jpg')
 
 Challenge.create!(title: 'Mache einen Schneeengel', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'lighthouse.jpg')
+}, image_url: 'snowboard.jpg')
 
 Challenge.create!(title: 'Erhalte 0.5% Rabatt auf jeden Kilometer in deinem Lauf', description: %{
 Mache einen Lauf und erhalte 0.5% Rabatt auf jeden Kilometer für das ganze Nike Sortiment
@@ -40,8 +40,8 @@ Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel mach
 
 Challenge.create!(title: 'Mache ein Selfie am Strand', description: %{
 Erhalte 15% Rabatt auf das ganze Mammut Sortiment wenn du einen Schneeengel machst
-}, image_url: 'run.jpg')
+}, image_url: 'lighthouse.jpg')
 
 Challenge.create!(title: 'Lorem Ipsum', description: %{
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-}, image_url: 'lighthouse.jpg')
+}, image_url: 'snowboard.jpg')
